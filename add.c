6aa1c7af813fb137caa2cf4f48f137b9b8168684
tejashwinikr <50978721+tejashwinikr@users.noreply.github.com>
2019-05-24 14:40:19 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,16 +1,29 @@
 #include<stdio.h>
-int add (int,int);
+int read();
+void display(int);
+int add(int,int);
 int main()
-{
-int a,b,c;
-printf("enter two numbers");
-scanf("%d %d",&a,&b);
-c=add(a,b);
-printf("sum=%d",c);
-}
+ {
+    int s=read();
+    display(s);
+ }
+int read()
+ {
+      int a,b,c;
+      printf("enter numbers to add");
+      scanf("%d%d",&a,&b);
+      c=add(a,b);
+      return c;
+ }
 int add(int a,int b)
 {
-int c;
-c=a+b;
-return c;
+      int c;
+      c=a+b;
+      return c;
 }
+void display(int c)
+{
+      printf("sum=%d",c);
+}
+
+
